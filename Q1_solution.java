0b3f70b01d92cd9eafe9332eb66b3b16e90614d8
Poorvaahuja <122693422+Poorvaahuja@users.noreply.github.com>
2024-01-04 08:57:29 +0530

    explained Q1_solution.java

--- Q1_solution.java
+++ Q1_solution.java
@@ -12,5 +12,5 @@ class Solution {
         }
         return ans;
     }
-    
 }
+// Initialized the value to be returned as zero(ans), iterating through the values in array, added the count in the hashmap, now for frequencies in the hashmap, added the ceil value in ans as if the count is not divisible by 2 or 3 still they can be removed by doing 3-4 operations. If the count of any key is 1, then there is no way to remove it, thus returned -1.
